bd8882f08f8394bd385d5074280dd85a9355c2ba
Bob Thompson <rothomps@mtu.edu>
2016-02-28 21:52:12 -0800

    Added Starter class

--- Cichlid Simulator/src/Game/Main.java
+++ Cichlid Simulator/src/Game/Main.java
@@ -7,6 +7,10 @@ import gameAssets.*;
 public class Main extends SimpleApplication {
 	private static Player player;
 	
+	public Main(){
+		
+	}//end of default constructor
+	
 	@Override
 	public void simpleInitApp() {
 		player = Player.getPlayer();
@@ -14,11 +18,6 @@ public class Main extends SimpleApplication {
 		System.out.println(player.getSize());
 		System.out.println(player.getSpeed());
 		System.out.println(player.getSex());
-	}
-
-	public static void main(String[] args) {
-		Main app = new Main();
-		app.start();
 	}
 
 }
---
+++ Cichlid Simulator/src/thinktank/simulator/Starter.java
@@ -0,0 +1,28 @@
+package thinktank.simulator;
+
+import Game.Main;
+
+/**
+ * 
+ * @author Bob
+ * @version %I%, %G%
+ */
+public class Starter{
+	//---------------------static constants----------------------------
+	//---------------------static variables----------------------------
+	private static Main client;
+	
+	//---------------------instance constants--------------------------
+	//---------------------instance variables--------------------------
+	//---------------------constructors--------------------------------
+	private Starter(){}//Starter is not to be instantiated
+	
+	//---------------------instance methods----------------------------
+	//---------------------static main---------------------------------
+	public static void main(String[] args){
+		client = new Main();
+		client.start();
+	}//end of main method
+	
+	//---------------------static methods------------------------------
+}//end of Starter class
