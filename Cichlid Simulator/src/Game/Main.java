a6213d873d074d61edfc67ac5d99633e21e4251f
jjcoffman <jjcoffman@me.com>
2016-03-05 11:31:34 -0800

    increased the camera speed and adjusted lighting brightness

--- Cichlid Simulator/src/Game/Main.java
+++ Cichlid Simulator/src/Game/Main.java
@@ -2,10 +2,13 @@ package Game;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.light.DirectionalLight;
+import com.jme3.light.Light;
 import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
 import com.jme3.texture.Texture;
+import com.jme3.util.SkyFactory;
 
 import gameAssets.*;
 import thinktank.simulator.environment.Tank;
@@ -27,13 +30,17 @@ public class Main extends SimpleApplication {
 		//load model of cichlid
 		makeSun();
 		makeCichlid(fish);
+		flyCam.setMoveSpeed(5);
 		
 		//printouts to test that player exists and has attributes
 		System.out.println(player.getSize());
 		System.out.println(player.getSpeed());
 		System.out.println(player.getSex());
+		
 		//initialize terrain, objects, other cichlids camera, and display.
 		makeTank(tank);
+		rootNode.attachChild(SkyFactory.createSky(
+	            assetManager, "horizon.jpg", false));
 	}
 	
 	private void makeTank(Tank tanks) {
@@ -45,9 +52,8 @@ public class Main extends SimpleApplication {
 	private void makeSun() {
 		DirectionalLight sun = new DirectionalLight();
         sun.setDirection(new Vector3f(-2f,-2f,-2f).normalizeLocal());
+        sun.setColor(new ColorRGBA(2,2,2,0));
         rootNode.addLight(sun);
-        
-         
 	}
 
 	private void makeCichlid(Spatial fish) {
