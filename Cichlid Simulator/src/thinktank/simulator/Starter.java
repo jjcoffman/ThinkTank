856e1843ffb48fba2d1d429b797673563ab622b8
josephmlai <josephmlai@10.117.149.147>
2016-03-10 10:22:44 -0800

    hdy

--- Cichlid Simulator/src/thinktank/simulator/Starter.java
+++ Cichlid Simulator/src/thinktank/simulator/Starter.java
@@ -1,29 +1,31 @@
 package thinktank.simulator;
 
 import Game.Main;
+
 //itworks
 /**
  * 
- * @author Bob Thompson
+ * @author Bob Thompsonfs
  * @version %I%, %G%
  */
-public class Starter{
-	//---------------------static constants----------------------------
-	//---------------------static variables----------------------------
+public class Starter {
+	// ---------------------static constants----------------------------
+	// ---------------------static variables----------------------------
 	private static Main client;
-	
-	//---------------------instance constants--------------------------
-	//---------------------instance variables--------------------------
-	//---------------------constructors--------------------------------
-	private Starter(){}//Starter is not to be instantiated
-	
-	//---------------------instance methods----------------------------
-	//---------------------static main---------------------------------
-	public static void main(String[] args){
+
+	// ---------------------instance constants--------------------------
+	// ---------------------instance variables--------------------------
+	// ---------------------constructors--------------------------------
+	private Starter() {
+	}// Starter is not to be instantiated
+
+	// ---------------------instance methods----------------------------
+	// ---------------------static main---------------------------------
+	public static void main(String[] args) {
 		client = new Main();
 		client.start();
-		
-	}//end of main method
-	
-	//---------------------static methods------------------------------
-}//end of Starter class
+
+	}// end of main method
+
+	// ---------------------static methods------------------------------
+}// end of Starter class
