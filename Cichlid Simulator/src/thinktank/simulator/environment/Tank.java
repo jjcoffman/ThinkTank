bd4b28155ef96d7c226cb4564148468a889b923c
Bob Thompson <rothomps@mtu.edu>
2016-03-11 10:47:24 -0800

    Small addition to Tank.

--- Cichlid Simulator/src/thinktank/simulator/environment/Tank.java
+++ Cichlid Simulator/src/thinktank/simulator/environment/Tank.java
@@ -25,38 +25,53 @@ import com.jme3.scene.Spatial;
 public class Tank{
 	//---------------------static constants----------------------------
 	//---------------------static variables----------------------------
+	private static Tank theTank;
+	
 	//---------------------instance constants--------------------------
 	//---------------------instance variables--------------------------
-
 	private Spatial tank;
-	private static Tank theTank;
+	private TANK_TYPE type;
+	
 	//---------------------constructors--------------------------------
-
-
 	private Tank(AssetManager manager){
 		super();
 		tank = manager.loadModel("Tank.obj");
 		scale(3);
 		tank.setQueueBucket(Bucket.Transparent); 
-		
-	}
+		type = TANK_TYPE.FIVE_GAL;
+	}//end of constructor
+	
+	//---------------------instance methods----------------------------
+	//GETTERS
 	public Spatial getSpatial(){
 		return tank;
-	}
+	}//end of getSpatial method
+	
+	public TANK_TYPE getType(){
+		return type;
+	}//end of getType method
+	
+	//SETTERS
 	public void setSpatial(Spatial spac){
 		tank = spac;
-	}
+	}//end of setSpactial method
+	
+	public void setType(TANK_TYPE type){
+		this.type = type;
+	}//end of setTYpe method
+	
+	//OPERATIONS
 	public void scale(float i){
 		tank.scale(i);
-	}
+	}//end of scale method
+	
+	//---------------------static main---------------------------------
+	//---------------------static methods------------------------------
 	//singleton
 	public static Tank getTank(AssetManager manager){
 		if(theTank == null)
 			theTank = new Tank(manager);
 		return theTank;
-	}
+	}//end of getTank method
 	
-	//---------------------instance methods----------------------------
-	//---------------------static main---------------------------------
-	//---------------------static methods------------------------------
 }//end of Tank class
