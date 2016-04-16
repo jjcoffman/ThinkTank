ed2dac01a0341d716edff0e79605f2424f3db61e
Bob Thompson <rothomps@mtu.edu>
2016-04-16 11:04:37 -0700

    Comments for Cichlid.

--- Cichlid Simulator/src/gameAssets/Cichlid.java
+++ Cichlid Simulator/src/gameAssets/Cichlid.java
@@ -41,6 +41,14 @@ import thinktank.simulator.entity.Fish;
 import thinktank.simulator.entity.IMoving;
 import thinktank.simulator.environment.Environment;
 
+/**
+ * Class representing a specific type of <code>Fish</code> object, which is a
+ * type of entity.
+ * 
+ * @author 
+ * @version %I%, %G%
+ *
+ */
 public class Cichlid extends Fish implements IMoving{
 	//---------------------static constants----------------------------
 	private static final long serialVersionUID = 8763564513637299079L;
@@ -52,19 +60,47 @@ public class Cichlid extends Fish implements IMoving{
 	//---------------------static variables----------------------------
 	//---------------------instance constants--------------------------
 	//---------------------instance variables--------------------------
-
+	/**
+	 * The value representing the gender of the cichlid.
+	 */
 	private String sex;
+	/**
+	 * The value representing the name of the cichlid.
+	 */
 	private String name;
+	/**
+	 * The behavior strategy used by this cichlid.
+	 */
 	private IStrategy strategy;
+	/**
+	 * 
+	 */
 	private AnimChannel channel;
+	/**
+	 * 
+	 */
 	private AnimControl control;
+	/**
+	 * 
+	 */
 	private CichlidController cc;
 
 	//---------------------constructors--------------------------------
+	/**
+	 * Constructor for creating a cichlid object with default values.
+	 */
 	public Cichlid(){
 		init();
 	}//end of default constructor
-	
+
+	/**
+	 * Constructor for creating a cichlid object with the specified values 
+	 * for size, speed, and sex.
+	 * 
+	 * @param size the size value for the cichlid.
+	 * @param speed the speed value for the cichlid.
+	 * @param sex the set value for the cichlid.
+	 */
 	public Cichlid(float size, float speed, String sex){
 		init();
 		setSpeed(speed);
@@ -74,29 +110,59 @@ public class Cichlid extends Fish implements IMoving{
 
 	//---------------------instance methods----------------------------
 	//GETTERS
+	/**
+	 * Returns the sex of the cichlid.
+	 * 
+	 * @return the sex of the cichlid.
+	 */
 	public String getSex(){
 		return sex;
 	}//end of getSex method
-	
+
+	/**
+	 * Returns the <code>IStrategy</code> used by the cichlid to determine 
+	 * its behavior.
+	 * 
+	 * @return the <code>IStrategy</code> used by the cichlid.
+	 */
 	public IStrategy getStrategy(){
 		return strategy;
 	}//end of getStrategy method
 	
 	//SETTERS
-	
+	/**
+	 * Sets the sex of this cichlid to the specified value.
+	 * 
+	 * @param sex the value to which the sex is to be set.
+	 */
 	public void setSex(String sex){
 		this.sex = sex;
 	}//end of setSex method
 	
+	/**
+	 * Sets the <code>IStrategy</code> to be used by this cichlid
+	 * to the specified object.
+	 * 
+	 * @param strat the object to use for this cichlid's behavior.
+	 */
 	public void setStrategy(IStrategy strat){
 		strategy = strat;
 	}//end of setStrategy method
 	
+	/**
+	 * Sets the name of this cichlid to the specified value.
+	 * 
+	 * @param name the value to which the name is to be set.
+	 */
 	public void setName(String name){
 		this.name = name;
-	}
+	}//end of setName method
 	
 	//OPERATIONS
+	/**
+	 * Initializes the values of this <code>Cichilid</code> object and 
+	 * prepares it to be displayed in the environment.
+	 */
 	private void init(){
 		setSpeed(1f);
 		sex = "male";
@@ -122,16 +188,33 @@ public class Cichlid extends Fish implements IMoving{
         
 	}//end of init method
 	
+	/**
+	 * Adds the specified <code>AbstractControl</code> object to the 
+	 * <code>Spatial</code> object representing this cichlid in the environment.
+	 * 
+	 * @param control the <code>AbstractControl</code> object to be added.
+	 */
 	public void addControl(AbstractControl control){
 		getObj().addControl(control);
 	}//end of addControl method
 	
+	/**
+	 * Calculates and sets the values for the dimensions of this 
+	 * cichlid in the environment.
+	 */
 	private void setDimensions(){
 		worldUnitDepth = Environment.inchesToWorldUnits(2.51969f);
 		float sizeFactor = worldUnitDepth / MODEL_DEPTH;
 		getObj().scale(sizeFactor);
 	}//end of setDimensions method
 
+	/**
+	 * Reads in the data for this serializable object.
+	 * 
+	 * @param stream the input data to be read
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
 	private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException{
 		init();
 		//values for Spatial local rotation
@@ -155,6 +238,12 @@ public class Cichlid extends Fish implements IMoving{
 		getObj().setLocalTransform(xform);
 	}//end of readObject method
 	
+	/**
+	 * Writes the data from this object to an output stream.
+	 * 
+	 * @param stream the output to be written to
+	 * @throws IOException
+	 */
 	private void writeObject(ObjectOutputStream stream) throws IOException{
 		//values for Spatial local rotation
 		stream.writeFloat(getObj().getLocalRotation().getX());
@@ -171,18 +260,31 @@ public class Cichlid extends Fish implements IMoving{
 		stream.writeFloat(getObj().getLocalTranslation().getZ());
 	}//end of writeObject method
 	
+	/**
+	 * Unused placeholder for object serialization.
+	 * @throws ObjectStreamException
+	 */
 	private void readObjectNoData() throws ObjectStreamException{}//end of readObjectNoData method
 
+	/**
+	 * NOT YET IMPLEMENTED
+	 */
 	@Override
 	public void onAnimChange(AnimControl arg0, AnimChannel arg1, String arg2) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * NOT YET IMPLEMENTED
+	 */
 	@Override
 	public void onAnimCycleDone(AnimControl control, AnimChannel channel, String anim) {
 	}
 
+	/**
+	 * NOT YET IMPLEMENTED
+	 */
 	@Override
 	public void move(double delta) {
 		
