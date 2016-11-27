373f251c3fb3d20497fc4227413be259b2e59c21
Bob Thompson <rothomps@mtu.edu>
2016-11-27 12:28:15 -0800

    Coding standards review for FishGhost

--- Cichlid Simulator/src/thinktank/simulator/entity/FishGhost.java
+++ Cichlid Simulator/src/thinktank/simulator/entity/FishGhost.java
@@ -2,21 +2,51 @@ package thinktank.simulator.entity;
 
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.control.GhostControl;
-import com.jme3.scene.Spatial;
 
-public class FishGhost extends GhostControl {
+/**
+ * 
+ * @author Bob
+ *
+ */
+public class FishGhost extends GhostControl{
+	//---------------------static constants----------------------------
+	//---------------------static variables----------------------------
+	//---------------------instance constants--------------------------
+	//---------------------instance variables--------------------------
+	/**
+	 * 
+	 */
 	private Fish owner;
 	
-	public FishGhost(CollisionShape ghostShape, Fish owner) {
+	//---------------------constructors--------------------------------
+	/**
+	 * 
+	 */
+	public FishGhost(){
+		super();
+		this.owner = null;
+	}//end of default constructor
+	
+	/**
+	 * 
+	 * @param ghostShape
+	 * @param owner
+	 */
+	public FishGhost(CollisionShape ghostShape, Fish owner){
 		super(ghostShape);
 		this.owner = owner;
-	}
-
-	public FishGhost() {
-		super();
-	}
-
+	}//end of (CollisionShape,Fish) constructor
+	
+	//---------------------instance methods----------------------------
+	//GETTERS
+	/**
+	 * 
+	 * @return
+	 */
 	public Fish getOwner(){
 		return owner;
-	}
-}
+	}//end of getOwner method
+	
+	//---------------------static main---------------------------------
+	//---------------------static methods------------------------------
+}//end of FishGhost class
