1307c9e720152782bb2e8fa96c4f23ab76ed31ce
Bob Thompson <rothomps@mtu.edu>
2016-11-27 12:10:35 -0800

    Coding standards review for CichlidController

--- Cichlid Simulator/src/thinktank/simulator/main/CichlidController.java
+++ Cichlid Simulator/src/thinktank/simulator/main/CichlidController.java
@@ -10,51 +10,107 @@ import com.jme3.math.Vector3f;
 
 import thinktank.simulator.Starter;
 
-public class CichlidController extends RigidBodyControl {
-	
-	private Vector3f moveDir;
-	private Quaternion viewDir;
-	private float moveSpeed, rotSpeed;
-	private boolean atLoc = false;
-
-	public CichlidController(CollisionShape shape, float mass, float moveSpeed, float rotSpeed) {
-		super(shape, mass);
-		this.moveSpeed = moveSpeed;
-		this.rotSpeed = rotSpeed;
-		Starter.getClient().getStateManager().getState(BulletAppState.class).getPhysicsSpace().addTickListener(physics);
-	}
-	
-	public void moveDirection(Vector3f direction){
-		moveDir = direction;
-	}
-	public void lookAt(Quaternion loc){
-		viewDir = loc;
-	}
-	
-	public void isAtLoc(boolean isAtLoc){
-		atLoc = isAtLoc;
-	}
-	PhysicsTickListener physics = new PhysicsTickListener(){
-
+/**
+ * 
+ * @author Bob
+ *
+ */
+public class CichlidController extends RigidBodyControl{
+	//---------------------static constants----------------------------
+	//---------------------static variables----------------------------
+	//---------------------instance constants--------------------------
+	//---------------------instance variables--------------------------
+	/**
+	 * 
+	 */
+	private PhysicsTickListener physics = new PhysicsTickListener(){
+		/**
+		 * 
+		 */
 		@Override
-		public void physicsTick(PhysicsSpace arg0, float arg1) {
-			// TODO Auto-generated method stub
-			
-		}
+		public void physicsTick(PhysicsSpace space, float tpf){
+		}//end of physicsTick method
 
+		/**
+		 * 
+		 */
 		@Override
-		public void prePhysicsTick(PhysicsSpace arg0, float arg1) {
-		    if (moveDir != null) {
+		public void prePhysicsTick(PhysicsSpace space, float tpf){
+		    if(moveDir != null){
 		    	setLinearVelocity(moveDir); 
 		    	moveDir = null;
 		    }
-		    if (atLoc){
+		    if(atLoc){
 		    	clearForces();
 		    	setLinearVelocity(Vector3f.ZERO);
 		    }
-		    
-			
-		}
-	};
-
-}
+		}//end of prePhysicsTick method
+	};//end of physics declaration
+	/**
+	 * 
+	 */
+	private Vector3f moveDir;
+	/**
+	 * 
+	 */
+	private Quaternion viewDir;
+	/**
+	 * 
+	 */
+	private float moveSpeed;
+	/**
+	 * 
+	 */
+	private float rotSpeed;
+	/**
+	 * 
+	 */
+	private boolean atLoc;
+	
+	//---------------------constructors--------------------------------
+	/**
+	 * 
+	 * @param shape
+	 * @param mass
+	 * @param moveSpeed
+	 * @param rotSpeed
+	 */
+	public CichlidController(CollisionShape shape, float mass, float moveSpeed, float rotSpeed){
+		super(shape, mass);
+		moveDir = null;
+		viewDir = null;
+		atLoc = false;
+		this.moveSpeed = moveSpeed;
+		this.rotSpeed = rotSpeed;
+		Starter.getClient().getStateManager().getState(BulletAppState.class).getPhysicsSpace().addTickListener(physics);
+	}//end of constructor
+	
+	//---------------------instance methods----------------------------
+	//OPERATIONS
+	/**
+	 * 
+	 * @param direction
+	 */
+	public void moveDirection(Vector3f direction){
+		moveDir = direction;
+	}//end of moveDirection method
+	
+	/**
+	 * 
+	 * @param loc
+	 */
+	public void lookAt(Quaternion loc){
+		viewDir = loc;
+	}//end of lookAt method
+	
+	/**
+	 * 
+	 * @param isAtLoc
+	 */
+	public void isAtLoc(boolean isAtLoc){
+		atLoc = isAtLoc;
+	}//end of isAtLoc method
+	
+	//---------------------static main---------------------------------
+	//---------------------static methods------------------------------
+}//end of CichlidController class
