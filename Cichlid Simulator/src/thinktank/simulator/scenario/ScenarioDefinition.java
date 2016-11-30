7474d1e231e83f95a61cd504847d8c9eb05e5a25
vashlor <vashlor@gmail.com>
2016-11-29 19:38:01 -0800

    Defined default scenarios

--- Cichlid Simulator/src/thinktank/simulator/scenario/ScenarioDefinition.java
+++ Cichlid Simulator/src/thinktank/simulator/scenario/ScenarioDefinition.java
@@ -2,6 +2,7 @@ package thinktank.simulator.scenario;
 
 import java.awt.Color;
 
+import com.bulletphysics.linearmath.Transform;
 import com.jme3.math.Quaternion;
 
 import thinktank.simulator.entity.Cichlid;
@@ -162,20 +163,20 @@ public final class ScenarioDefinition{
 	private void genEnts2(Scenario scenario){
 		//add fish
 		Cichlid fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.0019916727f, 0.11999998f, -0.119983464f);
+		fish.getObj().setLocalScale(0.0254f, 0.0254f, 0.0254f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		//add pots
 		Pot pot = EntityFactory.createPot();
-		//TODO set translate
-		//TODO set scale
-		//TODO set rotation
+		pot.getObj().setLocalTranslation(-2.73087E-4f, 0.0254f, -0.13999975f);
+		pot.getObj().setLocalScale(0.0762f, 0.0762f, 0.0762f);
+		pot.getObj().rotate(0, 90, 0);
 		scenario.addEnvironmentObject(pot);
 		//add plants
 		Plant plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(-0.05833201f, 0.0254f, 0.15098202f);
+		plant.getObj().setLocalScale(0.029526312f, 0.029542202f, 0.028008288f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 	}//end of getEnts2 method
@@ -187,25 +188,25 @@ public final class ScenarioDefinition{
 	private void genEnts3(Scenario scenario){
 		//add fish
 		Cichlid fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(0.051753357f, 0.08999999f, 0.14940417f);
+		fish.getObj().setLocalScale(0.0254f, 0.0254f, 0.0254f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.041285567f, 0.12999998f, -0.10952401f);
+		fish.getObj().setLocalScale(0.0254f, 0.0254f, 0.0254f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		//add pots
 		Pot pot = EntityFactory.createPot();
-		//TODO set translate
-		//TODO set scale
-		//TODO set rotation
+		pot.getObj().setLocalTranslation(0.044705704f, 0.0254f, 0.18138747f);
+		pot.getObj().setLocalScale(0.0762f, 0.0762f, 0.0762f);
+		pot.getObj().rotate(0, 90, 0);
+		//add pots
 		scenario.addEnvironmentObject(pot);
-		//add plants
 		Plant plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(-0.09556306f, 0.0254f, -0.152865f);
+		plant.getObj().setLocalScale(0.026418276f, 0.026432494f, 0.025060045f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 	}//end of getEnts3 method
@@ -217,90 +218,90 @@ public final class ScenarioDefinition{
 	private void genEnts4(Scenario scenario){
 		//add fish
 		Cichlid fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(0.052004013f, 0.22000003f, -0.20884176f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.1912249f, 0.14999999f, -0.17919688f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(0.068836f, 0.17f, -0.012712434f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.16350275f, 0.21000002f, 0.13589273f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		//add pots
 		Pot pot = EntityFactory.createPot();
-		//TODO set translate
-		//TODO set scale
+		pot.getObj().setLocalTranslation(0.012422485f, 0.0254f, 0.1294051f);
+		pot.getObj().setLocalScale(0.0762f, 0.0762f, 0.0762f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(pot);
 		//add plants
 		Plant plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(-0.15475345f, 0.0254f, -0.06125257f);
+		plant.getObj().setLocalScale(0.029526312f, 0.029542202f, 0.028008288f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(-0.12913f, 0.0254f, -0.26037896f);
+		plant.getObj().setLocalScale(0.03936841f, 0.0393896f, 0.03734438f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(-0.049622167f, 0.0254f, -0.23982519f);
+		plant.getObj().setLocalScale(0.01813019f, 0.018139949f, 0.01719807f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(0.0048237303f, 0.0254f, -0.35799065f);
+		plant.getObj().setLocalScale(0.031080326f, 0.031097054f, 0.029482407f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(0.13064393f, 0.0254f, -0.3425547f);
+		plant.getObj().setLocalScale(0.032116335f, 0.03213362f, 0.030465152f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(0.117809236f, 0.0254f, 0.077142775f);
+		plant.getObj().setLocalScale(0.032116335f, 0.03213362f, 0.030465152f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(0.13215373f, 0.0254f, -0.081457675f);
+		plant.getObj().setLocalScale(0.03367035f, 0.033688474f, 0.03193927f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(0.15309826f, 0.0254f, -0.22149652f);
+		plant.getObj().setLocalScale(0.026936281f, 0.026950778f, 0.025551418f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(-0.1495902f, 0.0254f, 0.21029106f);
+		plant.getObj().setLocalScale(0.032116335f, 0.03213362f, 0.030465152f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(-0.029375998f, 0.0254f, 0.32005772f);
+		plant.getObj().setLocalScale(0.019684205f, 0.0196948f, 0.01867219f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(0.10064329f, 0.0254f, 0.32980508f);
+		plant.getObj().setLocalScale(0.020720217f, 0.020731369f, 0.019654937f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(0.12152052f, 0.0254f, 0.21939106f);
+		plant.getObj().setLocalScale(0.021238223f, 0.021249654f, 0.020146312f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 	}//end of getEnts4 method
@@ -312,50 +313,50 @@ public final class ScenarioDefinition{
 	private void genEnts5(Scenario scenario){
 		//add fish
 		Cichlid fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.05202833f, 0.29f, -0.17689845f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(0.02867765f, 0.14999999f, -0.19224523f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(0.045390487f, 0.17f, 0.1550474f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.06315039f, 0.07999999f, 0.13458093f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		//add pots
 		Pot pot = EntityFactory.createPot();
-		//TODO set translate
-		//TODO set scale
-		//TODO set rotation
+		pot.getObj().setLocalTranslation(-0.06697098f, 0.0254f, -0.15720972f);
+		pot.getObj().setLocalScale(0.0762f, 0.0762f, 0.0762f);
+		pot.getObj().rotate(0, 160, 0);
 		scenario.addEnvironmentObject(pot);
 		pot = EntityFactory.createPot();
-		//TODO set translate
-		//TODO set scale
-		//TODO set rotation
+		pot.getObj().setLocalTranslation(0.0070289057f, 0.0254f, 0.15984556f);
+		pot.getObj().setLocalScale(0.0762f, 0.0762f, 0.0762f);
+		pot.getObj().rotate(0, 0, 0);
 		scenario.addEnvironmentObject(pot);
 		//add plants
 		Plant plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(-0.007075117f, 0.0254f, -0.08499807f);
+		plant.getObj().setLocalScale(0.036778387f, 0.036798183f, 0.034887515f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(-0.053520672f, 0.0254f, -0.009175407f);
+		plant.getObj().setLocalScale(0.023310244f, 0.02332279f, 0.022111805f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(0.02953006f, 0.0254f, 0.06259964f);
+		plant.getObj().setLocalScale(0.036778387f, 0.036798183f, 0.034887515f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 	}//end of getEnts5 method
@@ -367,75 +368,75 @@ public final class ScenarioDefinition{
 	private void genEnts6(Scenario scenario){
 		//add fish
 		Cichlid fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.05202833f, 0.29f, -0.17689845f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(0.02867765f, 0.14999999f, -0.19224523f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(0.045390487f, 0.17f, 0.1550474f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.06315039f, 0.07999999f, 0.13458093f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.023619981f, 0.109999985f, 0.086383976f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.08638396f, 0.08999999f, -0.023619983f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(0.024858873f, 0.27999999f, -0.047896907f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(0.047396127f, 0.04f, -0.16276915f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.16004182f, 0.31999996f, -0.29564947f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		fish = EntityFactory.createCichlid();
-		//TODO set translate
-		//TODO set scale
+		fish.getObj().setLocalTranslation(-0.17998411f, 0.14999999f, 0.30095136f);
+		fish.getObj().setLocalScale(0.0127f, 0.0127f, 0.0127f);
 		//TODO set rotation
 		scenario.addFish(fish);
 		//add pots
 		Pot pot = EntityFactory.createPot();
-		//TODO set translate
-		//TODO set scale
-		//TODO set rotation
+		pot.getObj().setLocalTranslation(0.1081209f, 0.0254f, 0.24085239f);
+		pot.getObj().setLocalScale(0.0762f, 0.0762f, 0.0762f);
+		pot.getObj().rotate(0, 0.5f, 0);
 		scenario.addEnvironmentObject(pot);
 		pot = EntityFactory.createPot();
-		//TODO set translate
-		//TODO set scale
-		//TODO set rotation
+		pot.getObj().setLocalTranslation(-0.10819905f, 0.0254f, -0.2308527f);
+		pot.getObj().setLocalScale(0.0762f, 0.0762f, 0.0762f);
+		pot.getObj().rotate(0, 10, 0);
 		scenario.addEnvironmentObject(pot);
 		//add plants
 		Plant plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(0.09116956f, 0.0254f, -0.14929199f);
+		plant.getObj().setLocalScale(0.036778387f, 0.036798183f, 0.034887515f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 		plant = EntityFactory.createPlant();
-		//TODO set translate
-		//TODO set scale
+		plant.getObj().setLocalTranslation(-0.11015297f, 0.0254f, 0.019139629f);
+		plant.getObj().setLocalScale(0.038332403f, 0.038353033f, 0.036361635f);
 		//TODO set rotation
 		scenario.addEnvironmentObject(plant);
 	}//end of getEnts6 method
